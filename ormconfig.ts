4d426ea4ca8ca489c80583b1f065c82052252496
AhmedKhchai <59939354+AhmedKhchai@users.noreply.github.com>
2022-04-23 13:46:04 +0200

    1st commit

---
+++ ormconfig.ts
@@ -0,0 +1,17 @@
+import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions';
+
+const config: MysqlConnectionOptions = {
+  type: 'mysql',
+  host: '127.0.0.1',
+  port: 3306,
+  username: 'root',
+  password: '1159951',
+  database: 'parkingNest',
+  entities: ['dist/src/**/*.entity.ts'],
+  synchronize: false,
+  migrations: ['dist/src/db/migrations/*.js'],
+  cli: {
+    migrationsDir: 'src/db/migrations',
+  },
+};
+export default config;
